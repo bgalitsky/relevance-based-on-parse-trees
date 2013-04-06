5964cf99ccd4ce3f1efefa71eb53feee841e5182
bg7550 <bg7550@gmail.com>
2013-04-06 00:41:56 +0000

    parse thicket

--- src/main/java/opennlp/tools/similarity/apps/SearchResultsProcessor.java
+++ src/main/java/opennlp/tools/similarity/apps/SearchResultsProcessor.java
@@ -26,7 +26,7 @@ import opennlp.tools.textsimilarity.ParseTreeChunkListScorer;
 import opennlp.tools.textsimilarity.SentencePairMatchResult;
 import opennlp.tools.textsimilarity.chunker2matcher.ParserChunker2MatcherProcessor;
 
-public class SearchResultsProcessor extends BingWebQueryRunner {
+public class SearchResultsProcessor extends BingQueryRunner {
   private static Logger LOG = Logger
       .getLogger("opennlp.tools.similarity.apps.SearchResultsProcessor");
   private ParseTreeChunkListScorer parseTreeChunkListScorer = new ParseTreeChunkListScorer();
@@ -90,26 +90,18 @@ public class SearchResultsProcessor extends BingWebQueryRunner {
   }
   
   public List<HitBase> runSearchViaAPI(String query) {
-    BingResponse resp = null, // obtained from bing
-    newResp = null; // re-sorted based on similarity
+	List<HitBase> hits = null;
     try {
-      List<String> resultList = search(query, "", "", 30);
-      resp = populateBingHit(resultList.get(0));
+      List<HitBase> resultList = runSearch(query);
       // now we apply our own relevance filter
-      newResp.setHits(calculateMatchScoreResortHits(resp.getHits(), query));
+      hits = calculateMatchScoreResortHits(resultList, query);
 
     } catch (Exception e) {
       // e.printStackTrace();
       LOG.info("No search results for query '" + query);
-      e.printStackTrace();
       return null;
     }
-    // cast to super class
-    List<HitBase> hits = new ArrayList<HitBase>();
-    for (HitBase h : resp.getHits())
-      hits.add((HitBase) h);
 
-    hits = removeDuplicates(hits, 0.9);
 
     return hits;
   }
