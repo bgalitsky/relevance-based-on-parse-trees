64c9240575f56b1eee56ec1f0cbc141ea647bc65
bg7550 <bg7550@gmail.com>
2013-09-29 15:31:45 +0000

    added mail and m-thread

---
+++ src/main/java/opennlp/tools/apps/contentgen/multithreaded/MyEventListener.java
@@ -0,0 +1,8 @@
+package opennlp.tools.apps.contentgen.multithreaded;
+
+import java.util.EventListener;
+
+
+public interface MyEventListener extends EventListener{
+	public void MyEvent(MyEvent evt);
+}
